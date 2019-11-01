6fd3cb2c98e2ffb1e4bbae1596369a6803164966
Ravindra Gupta <rkg_offc@yahoo.com>
2019-11-01 08:51:42 +0000

    updated for Linux

--- src/main/java/edurekaproject/FirstClass.java
+++ src/main/java/edurekaproject/FirstClass.java
@@ -2,6 +2,7 @@ package edurekaproject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -10,15 +11,22 @@ public class FirstClass {
 	public static SoftAssert st;
 	public static WebDriver driver;
   @Test
-  public void f() throws Throwable {
-	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-      driver=new ChromeDriver(); //open browser
+  public void testcase() throws Throwable{
+	  System.setProperty("webdriver.chrome.driver", "chromedriver");
+	  ChromeOptions chromeOptions = new ChromeOptions();
+	  chromeOptions.setHeadless(true);
+	  chromeOptions.addArguments("--headless");
+	  chromeOptions.addArguments("--no-sandbox");
+      driver=new ChromeDriver(chromeOptions); //open browser
       driver.manage().window().maximize(); //maximize browser window
-      driver.get("http://35.244.22.114:9191/website/");
+      driver.get("http://34.93.135.215:9001/website/");
       driver.findElement(By.xpath("//a[@id='About Us']")).click();
       verifySuccessMsg();
-      assertall();
       driver.close();
+      if(Test_fail){
+    	  st.assertAll();
+      }
+      
   }
   public void verifySuccessMsg() throws Throwable{
 	  try {
@@ -26,11 +34,13 @@ public class FirstClass {
 		  driver.findElement(By.xpath("//p[@id='PID-ab2-pg']")).isDisplayed();
 	  }catch(Throwable t){
 		  Test_fail = true;
+		  st= new SoftAssert();
+		  st.fail("All about page is not displayed");
+		  //driver.findElement(By.xpath("//*[contains(text(),'Apache')]")).isDisplayed();
 	  }
   }
-  public void assertall(){
-		if(Test_fail){
-		st.assertAll();
-		}
-	}
+	/*
+	 * public void assertall(){ if(Test_fail){ st.assertAll(); } }
+	 */
 }
+
